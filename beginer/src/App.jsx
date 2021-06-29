e86e99abba3ca3f21b3254de4a65e5e1b76d648e
Duoc <duthanhduoc@gmail.com>
2021-06-29 18:13:29 +0700

    update

--- beginer/src/App.jsx
+++ beginer/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Counter from './components/Counter/Counter'
 import Profile from './components/Profile/Profile'
-
+console.log(111)
 function App() {
   return (
     <div className="App">
